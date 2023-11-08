12137ebcd0fe07f0ef17be9fb0d5ea3d06eb4675
Daniel Phan <danielphan.2003@gmail.com>
2023-11-09 01:50:06 +0700

    fix(onboard): fix on resume parent lang picker

--- app/src/main/java/io/github/kdetard/koki/ui/screens/onboard/OnboardFragment.java
+++ app/src/main/java/io/github/kdetard/koki/ui/screens/onboard/OnboardFragment.java
@@ -22,15 +22,25 @@ public class OnboardFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_onboard, container, false);
+        return inflater.inflate(R.layout.fragment_onboard, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        View view = requireView();
+        final int id_langPickerBtn = R.id.onboardFragment_appBarLayout_langPickerBtn;
+
+        if (view.findViewById(id_langPickerBtn) == null) {
+            view = requireParent();
+        }
 
         RxView
-                .clicks(view.findViewById(R.id.onboardFragment_appBarLayout_langPickerBtn))
+                .clicks(view.findViewById(id_langPickerBtn))
                 .doOnNext(v -> Toast.makeText(requireContext(), "Function not implemented", Toast.LENGTH_SHORT).show())
                 .to(autoDisposable(AndroidLifecycleScopeProvider.from(this)))
                 .subscribe();
-
-        return view;
     }
 
     protected View requireParent() {
