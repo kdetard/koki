ae594b4f257c940fa3f2babcf1994cd1581d3767
Daniel Phan <danielphan.2003@gmail.com>
2023-11-12 12:11:47 +0700

    fix(onboard): landscape padding, clean up
    Signed-off-by: Daniel Phan <danielphan.2003@gmail.com>

--- app/src/main/java/io/github/kdetard/koki/ui/screens/onboard/OnboardFragment.java
+++ app/src/main/java/io/github/kdetard/koki/ui/screens/onboard/OnboardFragment.java
@@ -2,6 +2,7 @@ package io.github.kdetard.koki.ui.screens.onboard;
 
 import static autodispose2.AutoDispose.autoDisposable;
 
+import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
@@ -26,6 +28,8 @@ import io.github.kdetard.koki.ui.base.BaseFragment;
 
 @AndroidEntryPoint
 public class OnboardFragment extends BaseFragment {
+    private static final int LandscapeInsetType = WindowInsetsCompat.Type.displayCutout() | WindowInsetsCompat.Type.navigationBars();
+    private static final int PortraitInsetType = WindowInsetsCompat.Type.navigationBars() | WindowInsetsCompat.Type.ime();
     private static final int OutOfBoundInsetType = 10; /* WindowInsetsCompat.Type.SIZE + 1 */
 
     @Override
@@ -43,34 +47,49 @@ public class OnboardFragment extends BaseFragment {
                 .paddingTop(WindowInsetsCompat.Type.systemBars(), true)
                 .applyToView(view.findViewById(R.id.onboardFragment_appBarLayout));
 
-        setInsets(view.findViewById(R.id.onboardFragment_actionLayout));
+        setPortraitInsets(view.findViewById(R.id.onboardFragment_actionLayout));
 
         return view;
     }
 
-    private Insetter.Builder insetBuilder() {
-        return Insetter.builder()
-                .paddingBottom(WindowInsetsCompat.Type.navigationBars() | WindowInsetsCompat.Type.ime(), true);
-    }
+    private static void setPortraitInsets(View actionLayout) {
+        final Drawable background = actionLayout.getBackground();
 
-    private void setInsets(View parent) {
-        final Drawable background = parent.getBackground();
-        parent.addOnLayoutChangeListener((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> {
+        actionLayout.addOnLayoutChangeListener((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> {
             final int statusBarInsets = Objects.requireNonNull(ViewCompat.getRootWindowInsets(v))
                     .getInsets(WindowInsetsCompat.Type.statusBars()).top;
 
-            int topInsetType = OutOfBoundInsetType;
-            if (top <= statusBarInsets) {
-                topInsetType = WindowInsetsCompat.Type.statusBars();
-                v.setBackgroundColor(Objects.requireNonNull(v.getBackgroundTintList()).getDefaultColor());
-            } else {
-                v.setBackgroundColor(0);
-                v.setBackground(background);
-            }
+            final boolean isBehindStatusBar = top <= statusBarInsets;
+            final int topInsetType = isBehindStatusBar ? WindowInsetsCompat.Type.statusBars() : OutOfBoundInsetType;
 
-            insetBuilder()
+            toggleBackgroundTint(v, background, isBehindStatusBar);
+
+            Insetter.builder()
                     .paddingTop(topInsetType, true)
+                    .paddingBottom(PortraitInsetType, true)
                     .applyToView(v);
         });
+    }
+
+    private static void toggleBackgroundTint(View view, Drawable originalBackground, boolean pred) {
+        if (pred) {
+            view.setBackgroundColor(Objects.requireNonNull(view.getBackgroundTintList()).getDefaultColor());
+        } else {
+            view.setBackgroundColor(0);
+            view.setBackground(originalBackground);
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        final boolean isLandscape = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE;
+        final int orientationInsetType = isLandscape ? LandscapeInsetType : OutOfBoundInsetType;
+
+        Insetter.builder()
+                .paddingLeft(orientationInsetType, true)
+                .paddingRight(orientationInsetType, true)
+                .applyToView(requireActivity().findViewById(R.id.rootFragment_actionLayout));
     }
 }
